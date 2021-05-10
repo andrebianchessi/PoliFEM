195de403ee5dd078a17d1aedce729f917a4d50cc
andrebianchessi <andrebianchessi@gmail.com>
2021-05-10 12:23:49 -0300

    Line angle

--- src/models/line.ts
+++ src/models/line.ts
@@ -1,12 +1,17 @@
+import { Angle } from './angleInRadians'
 import { Node } from './node'
 
 export class Line {
     n1 : Node
     n2 : Node
+    angle: Angle
 
     constructor (n1:Node, n2:Node) {
         this.n1 = n1
         this.n2 = n2
+        const deltaY = n2.y - n1.y
+        const deltaX = n2.x - n1.x
+        this.angle = new Angle(Math.atan(deltaY / deltaX))
     }
 
     distance ():number {
@@ -14,6 +19,4 @@ export class Line {
         const deltaY = this.n2.y - this.n1.y
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY)
     }
-
-    horizontalAngle
 }

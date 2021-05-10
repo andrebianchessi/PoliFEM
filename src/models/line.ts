ba754946175cae604f80edef3221591c8b375c1e
andrebianchessi <andrebianchessi@gmail.com>
2021-05-10 12:04:55 -0300

    Line class

---
+++ src/models/line.ts
@@ -0,0 +1,17 @@
+import { Node } from './node'
+
+export class Line {
+    n1 : Node
+    n2 : Node
+
+    constructor (n1:Node, n2:Node) {
+        this.n1 = n1
+        this.n2 = n2
+    }
+
+    distance ():number {
+        const deltaX = this.n2.x - this.n1.x
+        const deltaY = this.n2.y - this.n1.y
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY)
+    }
+}

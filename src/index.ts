ec4b2a746124aac7fd373232603c67700c161f34
andrebianchessi <andrebianchessi@gmail.com>
2021-05-10 22:29:22 -0300

    plot inside Problem

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 /* eslint-disable no-new */
-// import { plot, Plot } from 'nodeplotlib'
-// const data: Plot[] = [{ x: [1, 3, 4, 5], y: [3, 12, 1, 4], text: ['a', 'b', 'c', 'd'], hoverinfo: 'text' }]
-// plot(data)
-
-import { plotElements } from './functions/plot'
 import { Element } from './models/element'
 import { Node } from './models/node'
 import { Problem } from './models/problem'
@@ -12,6 +7,7 @@ const p = new Problem()
 
 const n1 = Node.get(0, 0, p)
 const n2 = Node.get(0, 3, p)
-
 new Element('Frame', n1, n2, { E: 21 * 1000000000, I: 4.21875 / 100000, A: 0.0225 }, p)
-plotElements(p)
+
+p.build()
+p.plot()
--- src/models/problem.ts
+++ src/models/problem.ts
@@ -1,12 +1,38 @@
+import { Matrix } from 'mathjs'
 import { Element } from './element'
+import { math } from './math'
 import { Node } from './node'
+import { plot, Plot } from 'nodeplotlib'
 
 export class Problem {
     nodes: Map<number, Map<number, Node>>
     elements: Map<number, Element>
+    K?: Matrix
+    U?: Matrix
+    F?: Matrix
 
     constructor () {
         this.nodes = new Map<number, Map<number, Node>>()
         this.elements = new Map<number, Element>()
+    }
+
+    build () {
+        this.K = math.zeros!([this.nodes.size * 6, this.nodes.size * 6], 'sparse') as Matrix
+        this.U = math.zeros!([this.nodes.size * 6, 1], 'sparse') as Matrix
+        this.F = math.zeros!([this.nodes.size * 6, 1], 'sparse') as Matrix
+    }
+
+    plot () {
+        const x = []
+        const y = []
+        for (const [, e] of this.elements) {
+            x.push(e.n1.x)
+            y.push(e.n1.y)
+            x.push(e.n2.x)
+            y.push(e.n2.y)
+        }
+        // const data: Plot[] = [{ x: [1, 3, 4, 5], y: [3, 12, 1, 4], text: ['a', 'b', 'c', 'd'], hoverinfo: 'text' }]
+        const data: Plot[] = [{ x, y }]
+        plot(data)
     }
 }

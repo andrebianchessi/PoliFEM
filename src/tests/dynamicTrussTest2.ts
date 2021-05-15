722158e3a29a44de2547ce0126e6dfbc500cb5fa
andrebianchessi <andrebianchessi@gmail.com>
2021-05-15 01:44:16 -0300

    bridge right side ok

--- src/tests/dynamicTrussTest2.ts
+++ src/tests/dynamicTrussTest2.ts
@@ -15,22 +15,35 @@ import { Load } from '../models/load'
  */
 export function DynamicTrussTest2 () {
     console.log('Dynamic truss test 2')
-    const L = 100
+    const L = 100 // bridge length
+    const H = 20 // bridge height
     const properties = { E: 30 * 1000000, A: 1, rho: 7.4 / 10000 }
+
+    // Bridge top arch equation
+    function y (x: number):number {
+        const a = -4 * H / (L * L)
+        return a * x * x + H
+    }
 
     const p = new DynamicProblem()
 
-    let n1: Node
-    let n2: Node
+    const archNodes:Node[] = []
+    const floorNodes:Node[] = []
     const elementLength = L / 6
-    for (let i = 0; i <= 5; i++) {
-        n1 = Node.get(i * elementLength, 0, p)
-        n2 = Node.get((i + 1) * elementLength, 0, p)
-        new Element('Truss', n1, n2, properties, p)
+    let x = 0
+    for (let i = 0; i <= 3; i++) {
+        x = i * elementLength
+        floorNodes.push(Node.get(x, 0, p))
+        archNodes.push(Node.get(x, y(x), p))
     }
-
-    new BoundaryCondition(Node.get(0, 0, p), 'RollerX', p)
-    new BoundaryCondition(n2!, 'Pin', p)
+    for (let i = 0; i < floorNodes.length - 1; i++) {
+        new Element('Truss', floorNodes[i], floorNodes[i + 1], properties, p)
+        new Element('Truss', archNodes[i], archNodes[i + 1], properties, p)
+        new Element('Truss', floorNodes[i], archNodes[i], properties, p)
+    }
+    for (let i = 0; i < archNodes.length - 2; i++) {
+        new Element('Truss', archNodes[i], floorNodes[i + 1], properties, p)
+    }
 
     p.plot()
 

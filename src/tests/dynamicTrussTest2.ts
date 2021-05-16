43815e2d797053b0330d460d24512fd04e891428
andrebianchessi <andrebianchessi@gmail.com>
2021-05-16 12:33:18 -0300

    truss struct test

--- src/tests/dynamicTrussTest2.ts
+++ src/tests/dynamicTrussTest2.ts
@@ -9,13 +9,14 @@ import { Node } from '../models/node'
 import { Load } from '../models/load'
 import { StaticProblem } from '../models/staticProblem'
 import { PrintSparseMatrix } from '../functions/printSparseMatrix'
+import { BoundaryCondition } from '../models/boundaryCondition'
 
 /**
  * Finds truss bridge natural frequency
  */
 export function DynamicTrussTest2 () {
     console.log('Dynamic truss test 2')
-    const L = 6 // bridge length
+    const L = 600 // bridge length
     const H = 20 // bridge height
     const properties = { E: 1, A: 1, rho: 7.4 / 10000 }
 
@@ -33,31 +34,38 @@ export function DynamicTrussTest2 () {
     const f1 = Node.get(0, 0, p)
     const f2 = Node.get(elementLength, 0, p)
     const f3 = Node.get(elementLength * 2, 0, p)
-    const a1 = Node.get(elementLength, elementLength, p)
+    const a1 = Node.get(elementLength, y(elementLength), p)
     const a2 = Node.get(elementLength * 2, y(elementLength * 2), p)
 
     // floor
     new Element('Truss', f1, f2, properties, p)
-    // new Element('Truss', f2, f3, properties, p)
+    new Element('Truss', f2, f3, properties, p)
 
     // cross
-    // new Element('Truss', f1, a1, properties, p)
-    // new Element('Truss', f2, a2, properties, p)
+    new Element('Truss', f1, a1, properties, p)
+    new Element('Truss', f2, a2, properties, p)
 
     // vertical
     new Element('Truss', f2, a1, properties, p)
-    // new Element('Truss', f3, a2, properties, p)
+    new Element('Truss', f3, a2, properties, p)
 
     // top
-    // new Element('Truss', a1, a2, properties, p)
+    new Element('Truss', a1, a2, properties, p)
 
-    new Load(0, -1, 0, a1, p)
+    new Load(0, -1, 0, a2, p)
+    new BoundaryCondition(f1, 'Pin', p)
+    new BoundaryCondition(f3, 'Pin', p)
 
-    // p.plot(true)
-    p.solve()
-    p.plot(false, 1 / 100)
-    PrintSparseMatrix(p.K!)
-    PrintSparseMatrix(p.F!)
+    p.plot(true)
+    try {
+        p.solve()
+        p.plot(false, 1 / 10000)
+    } catch (e) {
+        console.log(e)
+    }
+
+    // PrintSparseMatrix(p.K!)
+    // PrintSparseMatrix(p.F!)
     // p.solveTimeHistory()
 
     // p.plotElementTension(p.elements.get(25)!)

fdb99e80e32b29c0967f826b565e20bfeb52aa90
andrebianchessi <andrebianchessi@gmail.com>
2021-05-16 12:01:18 -0300

    print sparse matrix

--- src/tests/dynamicTrussTest2.ts
+++ src/tests/dynamicTrussTest2.ts
@@ -4,21 +4,20 @@ Test of an axial impact on bar fixed o one end
 
 */
 
-import { BoundaryCondition } from '../models/boundaryCondition'
 import { Element } from '../models/element'
 import { Node } from '../models/node'
-import { DynamicProblem } from '../models/dynamicProblem'
 import { Load } from '../models/load'
 import { StaticProblem } from '../models/staticProblem'
+import { PrintSparseMatrix } from '../functions/printSparseMatrix'
 
 /**
  * Finds truss bridge natural frequency
  */
 export function DynamicTrussTest2 () {
     console.log('Dynamic truss test 2')
-    const L = 100 // bridge length
+    const L = 60 // bridge length
     const H = 20 // bridge height
-    const properties = { E: 30 * 1000000, A: 1, rho: 7.4 / 10000 }
+    const properties = { E: 1, A: 1, rho: 7.4 / 10000 }
 
     // Bridge top arch equation
     function y (x: number):number {
@@ -30,35 +29,34 @@ export function DynamicTrussTest2 () {
     const p = new StaticProblem()
 
     const elementLength = L / 6
-    const floorNodes: Node[] = []
-    const archNodes: Node[] = []
 
-    for (let i = 0; i < 7; i++) {
-        floorNodes.push(Node.get(i * elementLength, 0, p))
-    }
-    for (let i = 1; i < 6; i++) {
-        archNodes.push(Node.get(i * elementLength, y(i * elementLength), p))
-    }
+    const f1 = Node.get(0, 0, p)
+    const f2 = Node.get(elementLength, 0, p)
+    const f3 = Node.get(elementLength * 2, 0, p)
+    const a1 = Node.get(elementLength, y(elementLength), p)
+    const a2 = Node.get(elementLength * 2, y(elementLength * 2), p)
 
-    for (let i = 0; i < 6; i++) {
-        new Element('Truss', floorNodes[i], floorNodes[i + 1], properties, p)
-    }
-    for (let i = 0; i < 4; i++) {
-        new Element('Truss', archNodes[i], archNodes[i + 1], properties, p)
-    }
+    // floor
+    new Element('Truss', f1, f2, properties, p)
+    // new Element('Truss', f2, f3, properties, p)
 
-    new Element('Truss', floorNodes[0], archNodes[0], properties, p)
-    new Element('Truss', floorNodes[6], archNodes[4], properties, p)
+    // cross
+    // new Element('Truss', f1, a1, properties, p)
+    // new Element('Truss', f2, a2, properties, p)
 
-    for (let i = 1; i <= 5; i++) {
-        new Element('Truss', floorNodes[i], archNodes[i - 1], properties, p)
-    }
-    new Load(0, -100, 0, archNodes[2], p)
+    // vertical
+    new Element('Truss', f2, a1, properties, p)
+    // new Element('Truss', f3, a2, properties, p)
 
-    p.plot(true)
+    // top
+    // new Element('Truss', a1, a2, properties, p)
+
+    new Load(0, -1, 0, a1, p)
+
+    // p.plot(true)
     p.solve()
-    p.plot(false, 100000)
-
+    p.plot(false, 1 / 100)
+    PrintSparseMatrix(p.K!)
     // p.solveTimeHistory()
 
     // p.plotElementTension(p.elements.get(25)!)

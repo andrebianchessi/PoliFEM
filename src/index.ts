f54edcd78481293afe7127f1eb86659072618f4b
andrebianchessi <andrebianchessi@gmail.com>
2021-05-18 10:59:24 -0300

    tests rename

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { math } from './models/math'
-import { DynamicTrussTest1 } from './tests/trussAxialImpact'
-import { DynamicTrussTest2 } from './tests/dynamicTrussTest2'
-import { DynamicTrussTest3 } from './tests/fallingTruss'
-import { ModalTest } from './tests/simpleModal'
-import { StaticTrussTest } from './tests/staticTruss'
 
-// StaticTrussTest()
-// DynamicTrussTest1()
-// DynamicTrussTest2()
-DynamicTrussTest3()
-// ModalTest()
+import { BridgeModal } from './tests/bridgeModal'
+import { FallingTruss } from './tests/fallingTruss'
+import { StaticTruss } from './tests/staticTruss'
+import { TrussAxialImpact } from './tests/trussAxialImpact'
+
+StaticTruss()
+TrussAxialImpact()
+FallingTruss()
+BridgeModal()
+FallingTruss()

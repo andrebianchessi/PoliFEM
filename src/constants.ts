649c48a6ef5a8a32421101ea279ef7b747e9ba9f
andrebianchessi <andrebianchessi@gmail.com>
2021-05-19 18:29:27 -0300

    timesteps adjusted

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const bcColor = 'purple'
 export const initSpeedColor = 'orange'
 
 // number of steps to recalculate stiffness matrix
-export const timeStepsToRecalculateK = 500
+export const timeStepsToRecalculateK = 1000

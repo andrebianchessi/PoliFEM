2609956de1466d646c8a20f0663352b649e9ffc7
andrebianchessi <andrebianchessi@gmail.com>
2021-07-27 21:40:28 -0300

    index tests changed

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import { BeamDynamic } from './tests/beamDynamic'
 import { BeamModal } from './tests/beamModal'
+import { BeamStaticDiagram1 } from './tests/beamStaticDiagram1'
+import { BeamStaticDiagram2 } from './tests/beamStaticDiagram2'
+import { BeamStaticDiagram3 } from './tests/beamStaticDiagram3'
 import { BridgeDynamic } from './tests/bridgeDynamic'
 import { BridgeModal } from './tests/bridgeModal'
 import { BridgeStatic } from './tests/bridgeStatic'
 import { FallingBeam } from './tests/fallingBeam'
-import { StaticTruss } from './tests/staticTruss'
+import { FlatPlate } from './tests/flatPlate'
+import { FlatPlateHole } from './tests/flatPlateHole'
 import { TrussAxialImpact } from './tests/trussAxialImpact'
 
 const showPlots = true
-// StaticTruss(showPlots)
-// TrussAxialImpact(showPlots)
-// FallingBeam(showPlots)
-// BeamModal(showPlots)
-// BeamDynamic(showPlots)
+TrussAxialImpact(showPlots)
+FallingBeam(showPlots)
+BeamModal(showPlots)
 BridgeModal(showPlots)
-// BridgeStatic(showPlots)
-// BridgeDynamic(showPlots)
+BridgeStatic(showPlots)
+BridgeDynamic(showPlots)
+BeamStaticDiagram1(showPlots)
+BeamStaticDiagram2(showPlots)
+BeamStaticDiagram3(showPlots)
+FlatPlate()
+FlatPlateHole()
